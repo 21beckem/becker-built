bc8c994b271e515884b3a306046e6bda2618b731
Michael Becker <21beckem8@gmail.com>
2025-06-10 14:38:39 -0500

    Made SET blockly Block which has dropdowns for every component and for a bunch of things you could change on a component. But it doesn't save after refresh :/

--- blockly/blocks/page_components.js
+++ blockly/blocks/page_components.js
@@ -2,18 +2,33 @@
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
-
 const PAGE_COMPONENTS = JSON.parse(localStorage.getItem('pageComponents')) || {};
-console.log('PAGE_COMPONENTS', PAGE_COMPONENTS);
+
+// make list of all attributes that can be set or got on a component
+const ALL_COMP_ATTR = [
+    ['Background Color', 'style.backgroundColor'],
+    ['Font Size', 'style.fontSize'],
+    ['Width', 'style.width'],
+    ['Height', 'style.height'],
+    ['Text Color', 'style.color'],
+    ['Text', 'innerText'],
+    ['Visible', 'style.display']
+];
+
 
 function convertGrapesComponentsToBlockly(big_page_comps) {
     let cats = [];
+    let list = [];
     function nestedCategories(component) {
         let thisC = {
             kind: 'category',
             id: component.attributes.id,
             name: component['custom-name'] || component.type.toProperCase(),
         }
+        if (thisC.name && thisC.id) {
+            list.push( [thisC.name, thisC.id] );
+        }
+
         let myCats = [];
         if (component.components) {
             component.components.forEach(comp => {
@@ -21,7 +36,7 @@ function convertGrapesComponentsToBlockly(big_page_comps) {
                     myCats.push( nestedCategories(comp) );
                 }
             });
-            myCats.push( {kind: 'block', type: 'logic_boolean'} );
+            myCats.push( {kind: 'block', type: 'component_set_prop'} );
             thisC.contents = myCats;
         }
         return thisC;
@@ -30,46 +45,48 @@ function convertGrapesComponentsToBlockly(big_page_comps) {
         cats.push( nestedCategories(component) );
     });
     console.log('cats', cats);
-    return cats;
+    cats.unshift({
+        "kind": "category",
+        "name": "Tools",
+        "colour": 60,
+        "contents": []
+    });
+    return [ cats, list ];
 }
+const [ GrapeToolbox, GrapeDropdown ] = convertGrapesComponentsToBlockly(PAGE_COMPONENTS);
 
+// add all page components to the toolbar
 TOOLBOX.contents.push({
     "kind": "category",
     "name": "Page Components",
     "colour": 60,
-    "contents": convertGrapesComponentsToBlockly(PAGE_COMPONENTS)
+    "contents": GrapeToolbox
 });
 
-// ---- Fetch Block ----
+// ---- Set Block ----
 CUSTOM_BLOCKS.push({
-    "type": "fetch_url",
-    "tooltip": "Fetches data from a URL. The \"On Return\" section will run asynchronously!",
-    "helpUrl": "",
-    "message0": "Fetch %1 url %2 On Return %3 %4",
+    "type": "component_set_prop",
+    "tooltip": "",
+    "colour": 255,
+    "previousStatement": null,
+    "nextStatement": null,
+    "message0": "Set %1 . %2 to %3",
     "args0": [
         {
-            "type": "input_dummy",
-            "name": "dummy1"
+            "type": "field_dropdown",
+            "name": "NAME",
+            "options": GrapeDropdown
+        },
+        {
+            "type": "field_dropdown",
+            "name": "NAME",
+            "options": ALL_COMP_ATTR
         },
         {
             "type": "input_value",
-            "name": "url",
-            "check": "String"
-        },
-        {
-            "type": "field_variable",
-            "name": "returnedData",
-            "variable": "data"
-        },
-        {
-            "type": "input_statement",
-            "name": "onReturnStatement"
+            "name": "comp"
         }
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 60,
-    "inputsInline": true
+    ]
 });
 Blockly.JavaScript.forBlock['fetch_url'] = function (block, generator) {
     // TODO: change Order.ATOMIC to the correct operator precedence strength
